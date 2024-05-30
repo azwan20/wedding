290d6cc9b9ae67cd346360b02121e6fce6f519ec
Azwan Triyadi <80760932+azwan20@users.noreply.github.com>
2024-05-30 17:39:39 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -64,8 +64,8 @@ export default function Home() {
   return (
     <>
       <div className="home">
-        <audio ref={audioRef} src="dipertigamalam.mp3" loop autoplay />
         <div className="contentUtama">
+        <audio ref={audioRef} src="dipertigamalam.mp3" loop autoplay />
           <div className="sound" onClick={handleSound}>
             {!sound ? (
               <svg className="m-auto" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
